87ed8d57f0332dae79a1436797a658b58a8e8b2d
Matt Jaworski <matt@jwr.sk>
2016-10-18 11:08:53 +0800

    New PeepSoAjaxCallback implementation

--- classes/helloworldajax.php
+++ classes/helloworldajax.php
@@ -1,29 +1,10 @@
 <?php
 
-class PeepSoHelloworldAjax implements PeepSoAjaxCallback
+class PeepSoHelloworldAjax extends PeepSoAjaxCallback
 {
-	private static $_instance = NULL;
-
-	private function __construct() {}
-
-	public static function get_instance()
-	{
-		if (NULL === self::$_instance) {
-			self::$_instance = new self();
-		}
-		return (self::$_instance);
-	}
-
 	public function hello(PeepSoAjaxResponse $resp)
 	{
-		$peepsoInput = new PeepSoInput();
-		#$peepsoActivity = PeepSoActivity::get_instance();
-
-		#$user_id = get_current_user_id();
-		#$peepsoActivity->set_user_id($user_id);
-
-		$hello = $peepsoInput->get_int('hello', 'hello');
-
+		$hello = $this->_input->get_int('hello', 'hello');
 
 		$resp->success(TRUE);
 		$resp->set('hello', $hello);
