fe3cde1570b2ee9cfa00cb97d67dfba07bae9282
Matt Jaworski <matt@jwr.sk>
2017-05-08 15:11:26 +0800

    peepso/peepso#2017 PeepSoInput rename _i() to int()

--- classes/helloworldajax.php
+++ classes/helloworldajax.php
@@ -4,7 +4,7 @@ class PeepSoHelloworldAjax extends PeepSoAjaxCallback
 {
 	public function hello(PeepSoAjaxResponse $resp)
 	{
-		$hello = $this->_input->_i('hello', 'hello');
+		$hello = $this->_input->int('hello', 'hello');
 
 		$resp->success(TRUE);
 		$resp->set('hello', $hello);
